a1ca3b97034c272a88b12dc5d48f05a23b384c22
Nguyen Phat <phatnt.uit@gmail.com>
2021-02-09 16:53:04 +0700

    fix: fix test coverage

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app', () => {
   render(<App />);
   const app = screen.getByTestId('App');
   expect(app).toBeInTheDocument();
