0601cb8add231ce5c0cde0260067ae5b3cf26e09
Nguyen Phat <phatnt.uit@gmail.com>
2021-02-12 00:14:41 +0700

    feat: add bet logic & betting amount

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,24 @@ import { images, useRollDices, ImageName } from './utils';
 import './App.css';
 
 function App() {
-  const { names, rolling, needToShowResult, handleRoll } = useRollDices();
+  const {
+    names,
+    rolling,
+    needToShowResult,
+    betState,
+    amount,
+    handleRoll,
+    handleBet,
+    handleResetBet,
+  } = useRollDices();
 
   return (
     <div data-testid="App" className="App">
       <div className="side-section left-side">Happy new year 2021</div>
       <div className="side-section right-side">Happy new year 2021</div>
+      <div className="amount">
+        <span>${amount}</span>
+      </div>
       <div className="game-plate">
         {names.map((name, index) => (
           <div key={`${name}-${index}`} className="plate-item">
@@ -36,19 +48,40 @@ function App() {
         </button>
       </div>
       <div className="game-grid">
-        {Object.entries(images).map(([name, src]) => (
-          <div key={name} className={'game-item'}>
-            <div
-              className={`content ${
-                needToShowResult && names.includes(name as ImageName)
-                  ? 'matched'
-                  : ''
-              }`}
-            >
-              <img width={108} height="auto" src={src} alt={name} />
-            </div>
-          </div>
-        ))}
+        {(Object.entries(images) as Array<[ImageName, string]>).map(
+          ([name, src]) => {
+            const betted = betState[name] > 0;
+            return (
+              <div key={name} className={'game-item'} onClick={handleBet(name)}>
+                <div
+                  className={`content ${
+                    needToShowResult && names.includes(name) ? 'matched' : ''
+                  } ${betState[name] > 0 ? 'betted' : ''}`}
+                >
+                  {betted && (
+                    <>
+                      {!needToShowResult && (
+                        <div className="corner-section">
+                          <span
+                            className="reset"
+                            onClick={handleResetBet(name)}
+                          >
+                            &#x2715;
+                          </span>
+                        </div>
+                      )}
+                      <div className="bet-value">
+                        <span className="bet-value">{betState[name]}</span>
+                      </div>
+                    </>
+                  )}
+
+                  <img width={108} height="auto" src={src} alt={name} />
+                </div>
+              </div>
+            );
+          },
+        )}
       </div>
     </div>
   );
--- src/utils.ts
+++ src/utils.ts
@@ -24,12 +24,44 @@ const getRandomNames = () => {
     .map((index) => imageNames[index]);
 };
 
+const initiateBetState = () =>
+  imageNames.reduce((prev, name) => ({ ...prev, [name]: 0 }), {}) as Record<
+    ImageName,
+    number
+  >;
+
 export const useRollDices = () => {
   const [names, setNames] = useState<ImageName[]>(getRandomNames);
+  const [betState, setBetState] = useState(initiateBetState);
   const [rolling, setRolling] = useState(false);
   const [needToShowResult, setNeedToShowResult] = useState(false);
   const [intervalId, setIntervalId] = useState<number>();
   const [timeoutId, setTimeoutId] = useState<number>();
+  const [amount, setAmount] = useState(10);
+
+  const handleBet = useCallback(
+    (name: ImageName) => () => {
+      if (!rolling) {
+        const bettedItems = imageNames.filter((n) => betState[n] > 0);
+        // can bet only 3 items
+        if (bettedItems.length < 3 || bettedItems.includes(name)) {
+          setNeedToShowResult(false);
+          setBetState((prev) => ({ ...prev, [name]: prev[name] + 1 }));
+        }
+      }
+    },
+    [betState, rolling],
+  );
+
+  const handleResetBet = useCallback(
+    (name: ImageName) => (e: React.MouseEvent) => {
+      e.stopPropagation();
+      if (!rolling) {
+        setBetState((prev) => ({ ...prev, [name]: 0 }));
+      }
+    },
+    [rolling],
+  );
 
   const makeCleanInterval = useCallback(
     (id?: number) => () => {
@@ -46,12 +78,15 @@ export const useRollDices = () => {
       const id = window.setInterval(() => {
         setNames(getRandomNames());
       }, 100);
+      if (needToShowResult) {
+        setBetState(initiateBetState);
+      }
       setRolling(true);
       setNeedToShowResult(false);
       setIntervalId(id);
       setTimeoutId(window.setTimeout(makeCleanInterval(id), 3000));
     },
-    [makeCleanInterval],
+    [needToShowResult, makeCleanInterval],
   );
 
   useEffect(() => {
@@ -64,10 +99,32 @@ export const useRollDices = () => {
     };
   }, [makeCleanInterval, intervalId, timeoutId, rolling]);
 
+  // update amount
+  useEffect(() => {
+    if (!rolling && needToShowResult) {
+      const bettedItems = imageNames.filter((n) => betState[n] > 0);
+      const gainedAmount = bettedItems.reduce((total, item) => {
+        if (names.includes(item)) {
+          return total + betState[item];
+        }
+
+        return total - betState[item];
+      }, 0);
+      setAmount((prev) => prev + gainedAmount);
+      window.setTimeout(() => {
+        setBetState(initiateBetState);
+      }, 3000);
+    }
+  }, [rolling, betState, names, needToShowResult]);
+
   return {
     names,
     rolling,
     needToShowResult,
+    betState,
+    amount,
     handleRoll,
+    handleBet,
+    handleResetBet,
   };
 };
