646d3a9ebd2e8d317a5071eccba2c1eb79114cfe
Nguyen Phat <phatnt.uit@gmail.com>
2022-08-19 18:27:19 +0700

    feat: add i18n for missing button

--- src/components/new-game-button/index.tsx
+++ src/components/new-game-button/index.tsx
@@ -1,4 +1,5 @@
 import { memo } from 'react';
+import { useTranslation } from 'react-i18next';
 import { getStaticPath } from '../../helpers';
 import { PUBLIC_URL } from '../../constants';
 
@@ -6,21 +7,27 @@ import classes from './styles.module.css';
 
 export const NewGameButtonComponent: React.FC<NewGameButtonProps> = ({
   onClick,
-}) => (
-  <button
-    data-testid="new-game-button"
-    title="New game"
-    className={classes.root}
-    onClick={onClick}
-  >
-    <img
-      width={32}
-      height="auto"
-      src={getStaticPath('/images/sync.svg', PUBLIC_URL)}
-      alt="new-game-icon"
-    />
-  </button>
-);
+}) => {
+  const { t } = useTranslation();
+
+  return (
+    <button
+      data-testid="new-game-button"
+      title={t('new-game-txt', {
+        defaultValue: 'New game!',
+      })}
+      className={classes.root}
+      onClick={onClick}
+    >
+      <img
+        width={32}
+        height="auto"
+        src={getStaticPath('/images/sync.svg', PUBLIC_URL)}
+        alt="new-game-icon"
+      />
+    </button>
+  );
+};
 
 const NewGameButton = memo(NewGameButtonComponent);
 NewGameButton.displayName = 'NewGameButton';
